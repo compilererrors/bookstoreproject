127d12b3e075f961b659448e649a54f0dc3aa32f
Viktor <viktor.nyberg@hm.com>
2020-11-04 15:14:08 +0100

    added cart class

--- BookStore/src/main/java/com/example/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/BookStore/BookStoreApplication.java
@@ -1,5 +1,26 @@
 package com.example.BookStore;
 
+/*
+
+Requirements:
+
+- Create a Spring Boot MVC application with Thymeleaf as the View technology.
+
+- Use CSS to create a nice looking user interface with HTML and CSS.
+
+- Create at least one Unit Test that successfully tests some method in the application.
+
+- Use Git and GitHub and send a link to the GitHub repo to the teacher.
+
+- Send a Project description and your cool team name to the teacher. The Project description should include:
+
+Short description of the project
+- Webshop for books with a shop cart, user account page etc.
+Must-have features
+-
+Nice-to-have features.
+
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
---
+++ BookStore/src/main/java/com/example/BookStore/CartService.java
@@ -0,0 +1,4 @@
+package com.example.BookStore;
+
+public class CartService {
+}
