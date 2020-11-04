be1ba9ba23764b23428247f8064cc46781c52e15
Viktor <viktor.nyberg@hm.com>
2020-11-04 14:46:25 +0100

    added @bean in bookstoreapplication for testing

--- BookStore/src/main/java/com/example/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/BookStore/BookStoreApplication.java
@@ -2,12 +2,20 @@ package com.example.BookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 }
