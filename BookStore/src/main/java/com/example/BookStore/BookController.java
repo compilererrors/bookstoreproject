ea986a7e18177873cf9198438c1e710b7d7c91d6
HM\POJOH <pontus.johansson@hm.com>
2020-11-05 10:31:21 +0100

    cartUpdates

--- BookStore/src/main/java/com/example/BookStore/BookController.java
+++ BookStore/src/main/java/com/example/BookStore/BookController.java
@@ -20,6 +20,8 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+    @Autowired
+    private CartHandler cartHandler;
 
     @GetMapping("/")
     public String books(Model model, @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
@@ -102,21 +104,29 @@ public class BookController {
     }*/
 
     @RequestMapping(value = "/book/{page}/{id}/addBook", method = RequestMethod.POST)
-    public String rateHandler(HttpSession session, HttpServletRequest request, @ModelAttribute("book") Book book, Model model, Shopcart shopcart) {
-        List<Book> cart = (List<Book>) session.getAttribute("cart");
+    public String rateHandler(HttpSession session, HttpServletRequest request, @ModelAttribute("book") Book book, Model model) {
+        cartHandler.addItemToCart(book);
+
+        session.setAttribute("cartHandler", cartHandler.getCartItems());
 
 
-        if (cart == null) {
-            cart = new ArrayList<>();
-            session.setAttribute("cart", cart);
-        }
-        cart.add(book);
 
-        model.addAttribute("book", book);
         System.out.println(book.getTitle());
         String referer = request.getHeader("Referer");
         return "redirect:" + referer;
     }
+
+    @PostMapping("/addExistingBook")
+        public String addOneExistingBook(HttpSession session, HttpServletRequest request, int cartItemIndex){
+
+        cartHandler.addItemQTY(cartItemIndex);
+
+
+        return "shopcart";
+    }
+
+
+
 
     @GetMapping("/startpage")
     public String startpage(HttpSession session) {
@@ -162,10 +172,13 @@ public class BookController {
     }
 
     @GetMapping("/shopcart")
-    public String shopcart(HttpSession session, Shopcart shopcart) {
-
+    public String shopcart(HttpSession session) {
+        session.setAttribute("cartHandler", cartHandler.getCartItems());
         return "shopcart";
     }
+
+
+
 
     @GetMapping("/orders")
     public String orders(HttpSession session) {
