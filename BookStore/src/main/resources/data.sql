dbb853d5e9606e25d7a3dc3d0447baf851b8b005
HM\POJOH <pontus.johansson@hm.com>
2020-11-18 12:30:42 +0100

    adding a list of books + sql insert statements

---
+++ BookStore/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOK (TITLE, AUTHOR, PRICE) VALUES ('The Iliad', 'Homer', 300);
+INSERT INTO BOOK (TITLE, AUTHOR, PRICE) VALUES ('The Hitchhikers Guide to the Galaxy', 'Douglas Adams', 500);
+INSERT INTO BOOK (TITLE, AUTHOR, PRICE) VALUES ('Pippi Longstocking', 'Astrid Lindgren', 250);
