6e7d9a1a210278847d2e30b4d347321e366910de
HM\POJOH <pontus.johansson@hm.com>
2020-11-20 13:34:40 +0100

    small layout changes

--- BookStore/src/main/java/com/example/BookStore/BookService.java
+++ BookStore/src/main/java/com/example/BookStore/BookService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 public class BookService {
 
-    private static final int PAGE_SIZE = 10;
+    private static final int PAGE_SIZE = 8;
 
     @Autowired
     BookRepository bookRepository;
